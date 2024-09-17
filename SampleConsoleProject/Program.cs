f816da91a98f74c08b3f49256b887ed73386aa2e
Sumner Cotton <scotton@css.edu>
2024-09-17 11:03:43 -0500

    commit test line 1 and 2

--- SampleConsoleProject/Program.cs
+++ SampleConsoleProject/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Adding a line in Visual Studio");
+Console.WriteLine("test line 1");
 // added this line -- added to comment
-Console.WriteLine("Adding a line in VS Code");
+Console.WriteLine("Test line 2");
 
