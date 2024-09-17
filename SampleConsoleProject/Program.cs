8fbd4b5f1dc2ecbcc58ebc85add710ed70948623
Sumner Cotton <scotton@css.edu>
2024-09-17 11:06:47 -0500

    another test line has been addded

--- SampleConsoleProject/Program.cs
+++ SampleConsoleProject/Program.cs
@@ -3,4 +3,4 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("test line 1");
 // added this line -- added to comment
 Console.WriteLine("Test line 2");
-
+Console.WriteLine("Yet another test line")
